03b302a013bc44f06759ab2ae6ad71ccdbe63a8b
HCaupert <hcaupert@takima.fr>
2025-06-11 21:07:27 +0200

    fix: review notes display

--- src/app/(auth)/projects/[projectId]/reviews/[reviewId]/reviewNotes.tsx
+++ src/app/(auth)/projects/[projectId]/reviews/[reviewId]/reviewNotes.tsx
@@ -1,14 +1,11 @@
 import { ReviewerAvatar } from "@/lib/avatar/reviewer";
 import MarkdownRenderer from "@/components/ui/markdown-renderer";
 import {
-  type ReviewNote,
-  type Reviewer,
-  $Enums,
   type NoteDefinition,
+  type Reviewer,
+  type ReviewNote,
 } from "@prisma/client";
-import ReviewNoteType = $Enums.ReviewNoteType;
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { H3 } from "@/components/ui/typography";
 
 type ReviewNoteWithReviewerAndDefinition = ReviewNote & {
   reviewer: Reviewer;
@@ -22,26 +19,22 @@ interface ReviewNotesProps {
 export function ReviewNotes({ reviewNotes }: ReviewNotesProps) {
   if (!reviewNotes?.length) return null;
 
-  return (
-    <div className="flex gap-3">
-      {reviewNotes.map((note) => (
-        <Card key={note.id}>
-          <CardHeader className="flex-row items-center justify-between gap-2 space-y-0">
-            <ReviewerAvatar
-              reviewerName={note.reviewer.name}
-              options={{ size: 30, radius: 50 }}
-            />
-            <CardTitle>{note.reviewer.name}</CardTitle>
-            <div className="grow" />
-            <CardTitle>{note.noteDefinition.title}</CardTitle>
-          </CardHeader>
-          <CardContent className="text-sm">
-            <MarkdownRenderer>
-              {note.content.replace(/\\n/g, "\n")}
-            </MarkdownRenderer>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
-  );
+  return reviewNotes.map((note) => (
+    <Card key={note.id}>
+      <CardHeader className="flex-row items-center justify-between gap-2 space-y-0">
+        <ReviewerAvatar
+          reviewerName={note.reviewer.name}
+          options={{ size: 30, radius: 50 }}
+        />
+        <CardTitle>{note.reviewer.name}</CardTitle>
+        <div className="grow" />
+        <CardTitle>{note.noteDefinition.title}</CardTitle>
+      </CardHeader>
+      <CardContent className="text-sm">
+        <MarkdownRenderer>
+          {note.content.replace(/\\n/g, "\n")}
+        </MarkdownRenderer>
+      </CardContent>
+    </Card>
+  ));
 }
